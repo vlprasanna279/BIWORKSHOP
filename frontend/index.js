1a319b9aaa4430e77aaf193da1cbaaa6926056d8
vlprasanna279 <24wh1a05br@bvrithyderabad.edu.in>
2026-07-04 10:07:42 +0530

    message

--- frontend/index.js
+++ frontend/index.js
@@ -1,2 +1,47 @@
-console.log("hello from js");
-alert("hello from JS");
+const users = [
+    {
+        name: "Jane Doe",
+        gender: "Female",
+        image: "./images/jane.png"
+    },
+    {
+        name: "John Doe",
+        gender: "Male",
+        image: "./images/john.png"
+    }
+];
+
+let curUserId = 0;
+
+function toggle(){
+
+    if(curUserId === 0){
+        curUserId = 1;
+    }
+    else{
+        curUserId = 0;
+    }
+
+    document.getElementById("img").src = users[curUserId].image;
+    document.getElementById("card-name").innerText = users[curUserId].name;
+    document.getElementById("card-gender").innerText = users[curUserId].gender;
+}
+
+function getRandomUser(){
+
+    fetch("https://randomuser.me/api/")
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+
+        document.getElementById("img").src =
+        data.results[0].picture.large;
+
+        document.getElementById("card-name").innerText =
+        data.results[0].name.first + " " + data.results[0].name.last;
+
+        document.getElementById("card-gender").innerText =
+        data.results[0].gender;
+    });
+}
